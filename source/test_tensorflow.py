aed91d28a1f2936811ab91515b18dd7312654a62
48cfu <56238276+48cfu@users.noreply.github.com>
2019-12-29 20:31:28 +0100

    Starting point test tensor flow

---
+++ source/test_tensorflow.py
@@ -0,0 +1,22 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+# Create TensorFlow object called tensor
+hello_constant = tf.constant('Hello World!')
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+with tf.Session() as sess:
+    # Run the tf.constant operation in the session
+    output = sess.run(hello_constant)
+    print(output.decode())
+
+'''
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    print (sess.run(c))
+'''
